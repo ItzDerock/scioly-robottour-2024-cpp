67913d2e26d4876319b432c8d3aaf7b476fedd19
Derock <derock@derock.dev>
2024-03-19 20:24:04 -0400

    remove mpu9250

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include "quadrature_encoder.pio.h"
-#include "mpu9250/mpu9250.h"
 #include "hardware/i2c.h"
 
 const uint START_BUTTON_PIN = 15;
@@ -37,18 +36,7 @@ int main() {
 
 
   // setup MPU
-  i2c_init(i2c_default, 100 * 1000);
-  gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-  gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-  gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-  gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-  printf("i2c init success");
-  MPU9250* mpu = new MPU9250();
-  mpu->setup(0x68);
-  printf("mpu init, calibrating...");
-  mpu->calibrateMag();
-  printf("mpu ok!");
-
+ 
   // beep once  
   gpio_put(BEEPER_PIN, 1);
   sleep_ms(200);
